a8c55604f9965d26aff63a818642439d5da1cdc2
Julien Guay <julien.guay@insee.fr>
2019-12-04 17:37:08 +0100

    Initialisation du projet

---
+++ src/main/java/test/antlr/App.java
@@ -0,0 +1,13 @@
+package test.antlr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
